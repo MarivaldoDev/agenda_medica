339dc6734575681c5b1f098272c671c90eb7913c
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-07-20 22:33:47 -0300

    fix: corrigindo erro de digitação na variável

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
